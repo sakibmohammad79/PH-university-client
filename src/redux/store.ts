f5ab02f8235bb377fcdbf03c5616a56eb7b0361a
Sakib <sakibmohammad7679@gmail.com>
2024-01-18 23:25:27 +0600

    added serializeble object check

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
@@ -17,7 +26,11 @@ const store = configureStore({
     auth: persistAuthReducer,
   },
   middleware: (getDefaultMiddleWars) =>
-    getDefaultMiddleWars().concat(baseApi.middleware),
+    getDefaultMiddleWars({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(baseApi.middleware),
 });
 
 export default store;
