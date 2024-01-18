ecfdc3f1dde8530d6bf78a76b05b1535f147d9c8
Sakib <sakibmohammad7679@gmail.com>
2024-01-18 23:14:10 +0600

    setup redux persist for save userinfo in local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import { baseApi } from "./api/baseApi";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
-    auth: authReducer,
+    auth: persistAuthReducer,
   },
   middleware: (getDefaultMiddleWars) =>
     getDefaultMiddleWars().concat(baseApi.middleware),
@@ -17,3 +26,5 @@ export default store;
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
