e6770d38c3965c9c8ee86734295b857adae334cc
Sakib <sakibmohammad7679@gmail.com>
2024-02-03 17:44:24 +0600

    added antd select in academic semester

---
+++ src/components/form/PHSelect.tsx
@@ -0,0 +1,28 @@
+import { Form, Select } from "antd";
+import { Controller } from "react-hook-form";
+
+type TselectProps = {
+  name: string;
+  label: string;
+  options: { value: string; label: string; disabled?: boolean }[];
+};
+
+const PHSelect = ({ name, label, options }: TselectProps) => {
+  return (
+    <Controller
+      name={name}
+      render={({ field }) => (
+        <Form.Item label={label}>
+          <Select
+            defaultValue="lucy"
+            style={{ width: "100%" }}
+            {...field}
+            options={options}
+          />
+        </Form.Item>
+      )}
+    />
+  );
+};
+
+export default PHSelect;
--- src/pages/admin/academicManagement/CreateAcademicSemseter.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemseter.tsx
@@ -1,17 +1,37 @@
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
+import PHSelect from "../../../components/form/PHSelect";
+
+const nameOptions = [
+  {
+    value: "01",
+    label: "Spring",
+  },
+  {
+    value: "02",
+    label: "Summer",
+  },
+  {
+    value: "03",
+    label: "Fall",
+  },
+];
 
 const CreateAcademicSemseter = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     console.log(data);
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={12}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect name="name" label="Name: " options={nameOptions}></PHSelect>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
